d30c998badb41807e3c61bb2d00371911633aa16
Videh Nandini <videhnandini75@gmail.com>
2025-06-05 17:48:18 +0530

    Catalog service completed

---
+++ catalog/client.go
@@ -0,0 +1,81 @@
+package catalog
+
+import (
+	"Microservices-based-E-commerce-System/catalog/pb"
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.CatalogServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	c := pb.NewCatalogServiceClient(conn)
+	return &Client{conn, c}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
+
+func (c *Client) PostProduct(ctx context.Context, name, description string, price float64) (*Product, error) {
+	r, err := c.service.PostProduct(ctx, &pb.PostProductRequest{
+		Name:        name,
+		Description: description,
+		Price:       price,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &Product{
+		ID:          r.Product.Id,
+		Name:        r.Product.Name,
+		Description: r.Product.Description,
+		Price:       r.Product.Price,
+	}, nil
+}
+
+func (c *Client) GetProduct(ctx context.Context, id string) (*Product, error) {
+	r, err := c.service.GetProduct(ctx, &pb.GetProductRequest{
+		Id: id,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &Product{
+		ID:          r.Product.Id,
+		Name:        r.Product.Name,
+		Description: r.Product.Description,
+		Price:       r.Product.Price,
+	}, nil
+}
+
+func (c *Client) GetProducts(ctx context.Context, ids []string, query string, skip uint64, take uint64) ([]Product, error) {
+	r, err := c.service.GetProducts(ctx, &pb.GetProductsRequest{
+		Ids:   ids,
+		Take:  take,
+		Skip:  skip,
+		Query: query,
+	})
+	if err != nil {
+		return nil, err
+	}
+	products := []Product{}
+
+	for _, p := range r.Products {
+		products = append(products, Product{
+			ID:          p.Id,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		})
+	}
+	return products, nil
+}
